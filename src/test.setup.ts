da8e671d75f620207c593a475159c20ed3cbb2c8
lautaro <lautarogerezz12@gmail.com>
2026-05-14 15:53:23 -0300

    update: se modifico la version de angular a la 21 para funcionar con vitest

---
+++ src/test.setup.ts
@@ -0,0 +1,10 @@
+import '@analogjs/vitest-angular/setup-zone';
+import '@testing-library/jest-dom';
+import { getTestBed } from '@angular/core/testing';
+
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting(),
+);
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
+import analog from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
+    plugins: [analog()],
     test: {
         environment: 'jsdom',
         globals: true,
+        setupFiles: ['src/test.setup.ts'],
         include: ['src/**/*.spec.ts'],
     },
 });
